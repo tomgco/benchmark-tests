0e8355298aaa234144e4bf57db44011c02923503
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-09-24 16:03:18 +0100

    externalized running module

--- index.js
+++ index.js
@@ -1,37 +1,3 @@
-var arr = [];
-var ops = [sum, mul, div, date, push, shift];
+var run = require('./run');
 
-var op;
-function do_run_me_some() {
-  for (var i = 0 ; i < 4e8; i ++) {
-    op = ops[Math.floor(Math.random() * ops.length)];
-    op(Math.random() * 1000, Math.random() * 1000);
-  }
-}
-
-do_run_me_some();
-
-function sum(a, b) {
-  return a + b;
-}
-
-function mul(a, b) {
-  return a * b
-}
-
-function div(a, b) {
-  return a / b
-}
-
-function date() {
-  var d = new Date();
-  return d.toString();
-}
-
-function push(a) {
-  arr.push(a);
-}
-
-function shift() {
-  arr.shift();
-}
+run();
---
+++ run.js
@@ -0,0 +1,37 @@
+var arr = [];
+var ops = [sum, mul, div, date, push, shift];
+
+var op;
+function do_run_me_some() {
+  for (var i = 0 ; i < 4e8; i ++) {
+    op = ops[Math.floor(Math.random() * ops.length)];
+    op(Math.random() * 1000, Math.random() * 1000);
+  }
+}
+
+module.exports = do_run_me_some;
+
+function sum(a, b) {
+  return a + b;
+}
+
+function mul(a, b) {
+  return a * b
+}
+
+function div(a, b) {
+  return a / b
+}
+
+function date() {
+  var d = new Date();
+  return d.toString();
+}
+
+function push(a) {
+  arr.push(a);
+}
+
+function shift() {
+  arr.shift();
+}
