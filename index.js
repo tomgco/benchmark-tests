6cad9edd7f477a0d62d309e53b5779ba4127f981
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-09-24 15:35:45 +0100

    giving names to functions

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 var arr = [];
-var ops = [
-  function sum(a, b) {
-    return a + b;
-  },
-  function mul(a, b) {
-    return a * b
-  },
-  function div(a, b) {
-    return a / b
-  },
-  function date() {
-    var d = new Date();
-    return d.toString();
-  },
-  function push(a) {
-    arr.push(a);
-  },
-  function shift() {
-    arr.shift();
-  }
-];
+var ops = [sum, mul, div, date, push, shift];
 
 var op;
 for (var i = 0 ; i < 4e8; i ++) {
   op = ops[Math.floor(Math.random() * ops.length)];
   op(Math.random() * 1000, Math.random() * 1000);
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function mul(a, b) {
+  return a * b
+}
+
+function div(a, b) {
+  return a / b
+}
+
+function date() {
+  var d = new Date();
+  return d.toString();
+}
+
+function push(a) {
+  arr.push(a);
+}
+
+function shift() {
+  arr.shift();
 }
