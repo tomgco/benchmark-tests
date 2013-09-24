4f50328d4ee6a3738d19cb00e5d1dbb8535b8e6f
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-09-24 15:52:32 +0100

    wrapping with named function

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var arr = [];
 var ops = [sum, mul, div, date, push, shift];
 
 var op;
-for (var i = 0 ; i < 4e8; i ++) {
-  op = ops[Math.floor(Math.random() * ops.length)];
-  op(Math.random() * 1000, Math.random() * 1000);
+function do_run_me_some() {
+  for (var i = 0 ; i < 4e8; i ++) {
+    op = ops[Math.floor(Math.random() * ops.length)];
+    op(Math.random() * 1000, Math.random() * 1000);
+  }
 }
+
+do_run_me_some();
 
 function sum(a, b) {
   return a + b;
